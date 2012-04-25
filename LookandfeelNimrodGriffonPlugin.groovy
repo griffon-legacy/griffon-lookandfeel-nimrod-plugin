9a0eb3a314f78701beed7472fa05aff3503c6acb
aalmiray <aalmiray@gmail.com>
2012-04-25 11:51:51 +0200

    Updates for Griffon 0.9.5

--- LookandfeelNimrodGriffonPlugin.groovy
+++ LookandfeelNimrodGriffonPlugin.groovy
@@ -18,21 +18,50 @@
  * @author Andres Almiray
  */
 class LookandfeelNimrodGriffonPlugin {
-    def version = 0.3
-    def griffonVersion = '0.9.3 > *'
-    def dependsOn = [lookandfeel: 0.5]
-    def toolkits = ['swing']
-    def license = 'Apache Software License 2.0'
+    // the plugin version
+    String version = '0.4'
+    // the version or versions of Griffon the plugin is designed for
+    String griffonVersion = '0.9.5 > *'
+    // the other plugins this plugin depends on
+    Map dependsOn = [lookandfeel: '0.6']
+    // resources that are included in plugin packaging
+    List pluginIncludes = []
+    // the plugin license
+    String license = 'Apache Software License 2.0'
+    // Toolkit compatibility. No value means compatible with all
+    // Valid values are: swing, javafx, swt, pivot, gtk
+    List toolkits = ['swing']
+    // Platform compatibility. No value means compatible with all
+    // Valid values are:
+    // linux, linux64, windows, windows64, macosx, macosx64, solaris
+    List platforms = []
+    // URL where documentation can be found
+    String documentation = ''
+    // URL where source can be found
+    String source = 'https://github.com/griffon/griffon-lookandfeel-nimrod-plugin'
 
-    def author = 'Andres Almiray'
-    def authorEmail = 'aalmiray@users.sourceforge.net'
-    def title = 'NimROD Swing Look & Feel'
-    def description = '''
-NimROD Swing Look & Feel.
-http://personales.ya.com/nimrod/index-en.html
+    List authors = [
+        [
+            name: 'Andres Almiray',
+            email: 'aalmiray@yahoo.com'
+        ]
+    ]
+    String title = 'Pluggable Look & Feel based on NimROD'
+    String description = '''
+Pluggable Look & Feel based on [NimROD][1].
+
+Usage
+-----
+
+Refer to the [lookandfeel][2] plugin to find out how setup `lookAndFeel` and `theme` properties in the configuration.
+
+LookAndFeel and Themes
+----------------------
+| *LookAndFeel* | *Theme* |
+| ------------- | ------- |
+| Nimrod        | NimROD  |
+
+[1]: http://personales.ya.com/nimrod/index-en.html
+[2]: /plugin/lookandfeel
 '''
-
-    // URL to the plugin's documentation
-    def documentation = 'http://griffon.codehaus.org/Lookandfeel+Nimrod+Plugin'
 }
-
